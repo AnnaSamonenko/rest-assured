38e27dea633a15b87724b75e85c3b03a857eca69
Anna Samonenko <anna.samonenko@synapse.com>
2018-06-20 15:17:51 +0300

    Remove as a plus task

--- src/test/java/steps/flow/MarsPhotosSteps.java
+++ src/test/java/steps/flow/MarsPhotosSteps.java
@@ -1,6 +1,5 @@
 package steps.flow;
 
-import dto.CameraDTO;
 import dto.PhotoDTO;
 import resources.MarsPhotosEndpoint;
 
@@ -40,51 +39,6 @@ public class MarsPhotosSteps {
                 .stream()
                 .limit(quantity)
                 .map(PhotoDTO::getImg_src)
-                .collect(Collectors.toList());
-    }
-
-    public Map<String, Integer> getQuantityOfPhotosMadeByCameraInOrder(final String roverName, final int sol) {
-        Map<String, Integer> result =
-                getQuantityOfPhotosByCamera(roverName, sol)
-                        .entrySet()
-                        .stream()
-                        .sorted(Map.Entry.comparingByValue(Comparator.naturalOrder()))
-                        .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
-                                (oldValue, newValue) -> oldValue, LinkedHashMap::new));
-
-        return result;
-    }
-
-    private Map<String, Integer> getQuantityOfPhotosByCamera(final String roverName, final int sol) {
-        List<CameraDTO> cameraDTOS = getAllPhotos(roverName, sol)
-                .stream()
-                .map(PhotoDTO::getCamera)
-                .collect(Collectors.toList());
-
-        Set<String> nameOfCameras = cameraDTOS.stream().map(CameraDTO::getName).collect(Collectors.toSet());
-
-        Map<String, Integer> amountOfPhotos = new LinkedHashMap<>();
-
-        for (String key : nameOfCameras) {
-            amountOfPhotos.put(key, 0);
-        }
-
-        for (CameraDTO cameraDTO : cameraDTOS) {
-            for (String name : nameOfCameras)
-                if (cameraDTO.getName().equals(name)) {
-                    Integer amount = amountOfPhotos.get(name);
-                    amount++;
-                    amountOfPhotos.put(name, amount);
-                }
-        }
-
-        return amountOfPhotos;
-    }
-
-    private List<PhotoDTO> getAllPhotos(final String roverName, final int sol) {
-        return endpoint
-                .get(roverName, sol)
-                .stream()
                 .collect(Collectors.toList());
     }
 
--- src/test/resources/features/Image.feature
+++ src/test/resources/features/Image.feature
@@ -1,6 +1,6 @@
 Feature: Image
 
-  Scenario: 1 Image verification made by cameras by rover
+  Scenario: 1.0 Image verification made by cameras by rover
     When call Mars photos service with 1000 sol for curiosity rover for sol date for 5 photos
     And call Mars photos service with 1000 sol for curiosity rover for earth date for 5 photos
     Then metadata is the same
