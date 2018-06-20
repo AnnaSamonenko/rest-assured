cd7c50e5a8ee6ba760854d1fb4f5828564ddfd7c
Anna Samonenko <anna.samonenko@synapse.com>
2018-06-20 17:30:22 +0300

    Hooks added

---
+++ src/test/java/hooks/Hooks.java
@@ -0,0 +1,21 @@
+package hooks;
+
+import cucumber.api.java.After;
+import org.apache.commons.io.FileUtils;
+import org.apache.log4j.Logger;
+import utils.PropertyReader;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Hooks {
+
+    private Logger logger = Logger.getLogger(Hooks.class);
+
+    @After
+    public void deleteDirectories() throws IOException {
+        FileUtils.deleteDirectory(new File(PropertyReader.getProperty("photos.earth.date.dir")));
+        FileUtils.deleteDirectory(new File(PropertyReader.getProperty("photos.sol.dir")));
+        logger.info("Directories deleted");
+    }
+}
