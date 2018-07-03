d8c20483c2e4b0383e7ceeab9d37a3b61f117cf6
Anna Samonenko <anna.samonenko@synapse.com>
2018-07-03 13:43:57 +0300

    Added plugin

--- src/main/java/TestRunner.java
+++ src/main/java/TestRunner.java
@@ -1,6 +1,9 @@
+import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+import net.thucydides.core.annotations.WithTag;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
+@CucumberOptions(features="src/test/resources/features/Image.feature", tags={"@See"})
 public class TestRunner {
 }
